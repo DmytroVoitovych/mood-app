3311e08b332e0ce9ef520725e2ef6a24e64cdb4c
DmytroVoitovych <begunec23@gmail.com>
2025-05-25 23:14:30 +0300

    feat: enhance onboarding flow by adding route watching and global profile state management

--- src/pages/auth/OnBoarding.vue
+++ src/pages/auth/OnBoarding.vue
@@ -11,7 +11,20 @@
 <script setup lang="ts">
 import AuthForm from "~/components/shared/AuthForm.vue";
 import { updateUserProfile } from "~/composables/updateUserProfile";
-import { useRouter } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+import { useGlobalProfileState } from "~/composables/globalProfileState";
+import { watch } from "vue";
 
 const router = useRouter();
+const route = useRoute();
+const state = useGlobalProfileState();
+
+watch(
+  () => state.value.isAboard,
+  (n) => {
+    if (n && route.name === "/auth/OnBoarding") {
+      router.push({ name: "/" });
+    }
+  },
+);
 </script>
